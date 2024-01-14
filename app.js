e1c5ec41b5aa7989bbe92b0c115c0ee010f227fe
Diegu <diegudeveloper@gmail.com>
2024-01-14 16:42:58 +0100

    configuracion fetch para obtener el nombre y la imagen de la pagina

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const cargarPelicula = async () => {
     
     try {
-        const respuesta = await fetch ("http://api.themoviedb.org/3/movie/550?api_key=70cd2641c242096a334e9fbbf541e90c&language=es-ES")
+        const respuesta = await fetch ("http://api.themoviedb.org/3/movie/popular?api_key=70cd2641c242096a334e9fbbf541e90c&language=es-ES")
         
         console.log(respuesta);
 
         if (respuesta.status === 200) {
             const data = await respuesta.json();
-            console.log(data.title);
+            console.log(data.results);
+
+            let peliculas = "";
+
+            data.results.forEach(pelicula => {
+                peliculas += `
+                    <div class="pelicula">
+                        <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                        <h3 class="titulo">${pelicula.title}</h3>
+                    </div>
+                `;
+            });
+
+            document.getElementById("contenedor").innerHTML = peliculas;
             
         } else if (respuesta.status === 401){
             console.log("Existe un error de autenticacion")
