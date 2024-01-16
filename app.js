8c79af51921035cd1c5183e1342d1ed90b4045d7
Diegu <diegudeveloper@gmail.com>
2024-01-16 15:45:47 +0100

    Se configura la capa del hero para darle un toque mas dinamico

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const cargarBanner = async () => {
                 <div class="heroImage">
                     <img class="poster" src="https://image.tmdb.org/t/p/w500/${banner.backdrop_path}">
                     <h2 class="titleBanner">${banner.title}</h2>
+                    <div class="capa"></capa>
                 </div>
             `;
 
